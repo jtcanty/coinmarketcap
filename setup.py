ee328ee3bd76ac7b330a52db5b592c7e9df5ade9
John Canty <jtcanty117@gmail.com>
2018-02-11 02:00:37 -0800

    Added setup, init, and requirements files

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(setup(name='coinmarketcap',
+      version='1.0',
+      description='CoinMarketCap API Scraper',
+      author='John Canty',
+      author_email='jtcanty117@gmail.com',
+      url='https://github.com/jtcanty/coinmarketcap',
+      packages=['coinmarketcap'],
+      install_requires=['requests']
+     )
